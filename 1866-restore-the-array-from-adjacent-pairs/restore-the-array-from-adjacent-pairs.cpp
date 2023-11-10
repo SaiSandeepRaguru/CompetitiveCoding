4eb8b858b3f1a1d304fe2e65b16726161ba5158e
Raguru Sai Sandeep <62465620+SaiSandeepRaguru@users.noreply.github.com>
2023-11-10 12:58:48 +0530

    Time: 530 ms (28.18%) | Memory: 115.4 MB (71.52%) - LeetSync

--- 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.cpp
+++ 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     vector<int> restoreArray(vector<vector<int>>& ap) {
+        //Initialize a graph, where graph[node] holds a list of neighbors for node.
         map<int,vector<int>> graph;
+
+        /*Iterate over each edge (x, y) in adjacentPairs:
+            Add y to graph[x].
+            Add x to graph[y].*/
         for(vector<int> vt: ap)
         {
             int x=vt[0];
@@ -15,6 +20,9 @@ public:
             else
             graph[y].push_back(x);
         }
+
+        /*Iterate over each num in graph:
+            If the length of graph[num] is equal to 1, set root = num and break from the loop.*/
         int start;
         for(auto i=graph.begin();i!=graph.end();i++)
         {
@@ -24,6 +32,12 @@ public:
                 break;
             }
         }
+
+        /*Define a function dfs(node, prev, ans):
+                Add node to ans.
+                Iterate over each neighbor in graph[node]:
+                    If neighbor != prev:
+                        Call dfs(neighbor, node, ans).*/
         vector<int> res;
         res.push_back(start);
         int prev=-999999999;
