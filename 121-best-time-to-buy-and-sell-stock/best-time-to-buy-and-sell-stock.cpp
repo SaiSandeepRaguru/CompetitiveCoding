2746bc4960b02c7872a267b71d428c47af5b637e
Raguru Sai Sandeep <62465620+SaiSandeepRaguru@users.noreply.github.com>
2025-02-13 19:18:50 +0530

    Time: 0 ms (100.00%) | Memory: 97.5 MB (7.09%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,23 +1,37 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         vector<int> mini,maxi;
+//         int res=0;
+//         int start=0,end=prices.size()-1;
+//         int c=prices[0],d=prices[end];
+//         for(int i=0;i<prices.size();i++){
+//             c=min(c,prices[i]);
+//             mini.push_back(c);
+//         }
+//         for(int i=prices.size()-1;i>=0;i--){
+//             d=max(d,prices[i]);
+//             maxi.push_back(d);
+//         }
+//         reverse(maxi.begin(),maxi.end());
+//         for(int i=start;i<=end;i++){
+//             res=max(res,maxi[i]-mini[i]);
+//         }
+//         return res;
+        
+//     }
+// };
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<int> mini,maxi;
-        int res=0;
-        int start=0,end=prices.size()-1;
-        int c=prices[0],d=prices[end];
-        for(int i=0;i<prices.size();i++){
-            c=min(c,prices[i]);
-            mini.push_back(c);
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            minPrice = min(minPrice, price);  // Update min price seen so far
+            maxProfit = max(maxProfit, price - minPrice);  // Max profit if sold today
         }
-        for(int i=prices.size()-1;i>=0;i--){
-            d=max(d,prices[i]);
-            maxi.push_back(d);
-        }
-        reverse(maxi.begin(),maxi.end());
-        for(int i=start;i<=end;i++){
-            res=max(res,maxi[i]-mini[i]);
-        }
-        return res;
-        
+
+        return maxProfit;
     }
 };
