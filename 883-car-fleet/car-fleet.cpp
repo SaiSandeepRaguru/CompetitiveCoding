1750fafb7b187f6b5edfceed4e0ac96e4d87c1d4
Raguru Sai Sandeep <62465620+SaiSandeepRaguru@users.noreply.github.com>
2025-02-27 00:24:12 +0530

    Time: 793 ms (7.04%) | Memory: 82.4 MB (68.40%) - LeetSync

---
+++ 883-car-fleet/car-fleet.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int,float>> vp;
+        for(int i=0;i<position.size();i++){
+            vp.push_back({position[i],(target-position[i])/(1.0*speed[i])});
+        }
+        sort(vp.begin(),vp.end(),greater<pair<int,int>>());
+        int car_fleet_count=1;
+        float fleet_speed = vp[0].second;
+        for(int i=1;i<position.size();i++){
+            //compare speeds
+            cout<<vp[i].second<<endl;
+            if(vp[i].second > fleet_speed){
+                car_fleet_count++;
+                fleet_speed = vp[i].second;
+            }
+        }
+        return car_fleet_count;
+        
+    }
+};
