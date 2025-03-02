21a264562a9c979ce4e7604162637b91067753a3
Raguru Sai Sandeep <62465620+SaiSandeepRaguru@users.noreply.github.com>
2025-03-02 23:43:48 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (27.98%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,51 +1,41 @@
 class Solution {
-    
 public:
-
+    vector<vector<int>> dir = {{-1,0},{0,-1},{1,0},{0,1}};
     int orangesRotting(vector<vector<int>>& grid) {
-        vector<vector<int>> dir={{-1,0},{0,-1},{1,0},{0,1}};
         queue<int> q;
-        int n=grid.size(),m=grid[0].size();
-        int visited[n][m];
-        memset(visited,-1,sizeof(visited));
-        int time=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
+        int r=grid.size(),c=grid[0].size();
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
                 if(grid[i][j]==2){
-                    q.push(i*m+j);
-                    visited[i][j]=0;
+                    q.push(i*c+j);
                 }
             }
         }
+        int level=0; 
         while(!q.empty()){
-            int sz=q.size();
-            int flag=0;
-            for(int i=0;i<sz;i++){
-                int top=q.front(); q.pop();
-                int xc=top/m;
-                int yc=top%m;
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                int x=q.front(); q.pop();
+                int xr = x/c,yr=x%c;
                 for(int j=0;j<4;j++){
-                    int nxc = xc + dir[j][0];
-                    int nyc = yc + dir[j][1];
-                    if(nxc>=0 && nxc<n && nyc>=0 && nyc<m && visited[nxc][nyc]==-1 && grid[nxc][nyc]==1){
-                        flag=1;
-                        visited[nxc][nyc]=0;
-                        q.push(nxc*m+nyc);
+                    int nx = xr+dir[j][0];
+                    int ny = yr+dir[j][1];
+                    if(nx>=0 && ny>=0 && nx<r && ny<c && grid[nx][ny]==1){
+                        grid[nx][ny]=2;
+                        q.push(nx*c + ny);
                     }
                 }
             }
-            if(flag==1)
-            time++;
+            if(!q.empty())
+            level++;
         }
-        int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(visited[i][j]==-1 && grid[i][j]==1){
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]==1){
                     return -1;
                 }
             }
         }
-        return time;
-        
+        return level;
     }
 };
